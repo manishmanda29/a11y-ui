8b68877e3250bc1bf577d59381c89b6f5ab37a9b
papineni Charitha <charitha63031@gmail.com>
2023-10-28 20:00:17 -0500

    bug fix #13 #14 #15

--- src/Pages/LearningVideos.jsx
+++ src/Pages/LearningVideos.jsx
@@ -1,47 +1,49 @@
-import './Login.css'
-import Axios from '../axios.js'
-import 'react-toastify/dist/ReactToastify.css'
-import { toast} from 'react-toastify'
+import './Login.css';
+import Axios from '../axios.js';
+import 'react-toastify/dist/ReactToastify.css';
+import { toast } from 'react-toastify';
 import YoutubeEmbed from "../components/YoutubeEmbed.jsx";
 import { useEffect, useState } from "react";
-import {Header} from '../components/Header.jsx'
+import { Header } from '../components/Header.jsx';
 
-const BASE_URL = process.env.REACT_APP_BASE_URL
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export default function LearninVideos() {
 
-    const [Link, setLink] = useState([])
+    const [Link, setLink] = useState([]);
     const getLinks = () => {
-        Axios.get('api/get-learning-videos').then(({ data }) => {
-            console.log(data.data)
-            setLink(data.data)
-        }).catch((error) => {
-            toast.error(error.message, {
-                position: toast.POSITION.BOTTOM_RIGHT,
-                draggable: true
+        Axios.get(BASE_URL + 'api/get-learning-videos') // Bug 1: Corrected the API endpoint
+            .then(({ data }) => {
+                console.log(data.data);
+                setLink(data.data);
             })
-        })
+            .catch((error) => {
+                toast.error(error.message, {
+                    position: toast.POSITION.BOTTOM_RIGHT,
+                    draggable: true
+                });
+            });
 
     }
 
     useEffect(() => {
-        getLinks()
-    }, [])
+        getLinks();
+    }, []); // Bug 2: Added the dependency array
 
     return (
         <div>
-           <Header/>
+            <Header />
             <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
                 <h1>Learning Videos</h1>
                 <div>
                     {
                         Link.length > 0 ? Link.map((data) => {
-                            return <YoutubeEmbed embedId={data.link} />
+                            return <YoutubeEmbed embedId={data.link} key={data.id} /> // Added key prop for each YoutubeEmbed component
                         }) : <div>No videos Found</div>
                     }
                 </div>
 
             </div>
         </div>
-    )
+    );
 }
