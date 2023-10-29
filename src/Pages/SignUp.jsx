89f1588dae20c2bb4caa6021b22d96306b251621
charithapapineni <charithapapineni@gmail.com>
2023-10-28 20:35:29 -0500

    bug fix #16 #17 #18

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -9,35 +9,36 @@ import {Link,useNavigate} from 'react-router-dom'
 
 export const SignUp = () => {
     let navigate = useNavigate();
-    const register=(values, { setSubmitting }) => {
-        console.log("i am hereee")
+    const register = (values, { setSubmitting }) => {
+        console.log("I am here");
 
-        axios.post(process.env.REACT_APP_BASE_URL+'api/auth/register',{email:values.email,password:values.password,username:values.username}).then((data)=>
-        {
-            toast.success("Successfull Registered",{
+        axios.post(`${process.env.REACT_APP_BASE_URL}api/auth/register`, {
+            email: values.email,
+            password: values.password,
+            username: values.username
+        }).then((data) => {
+            toast.success("Successfully Registered", {
                 position: toast.POSITION.BOTTOM_RIGHT,
                 draggable: true
-            })
-           setTimeout(()=>{
-            navigate('/login')
-           },2000)        
-
-        }).catch((error)=>{
+            });
+            setTimeout(() => {
+                navigate('/login')
+            }, 2000);
+        }).catch((error) => {
             toast.error(error.message, {
                 position: toast.POSITION.BOTTOM_RIGHT,
                 draggable: true
-              })
-        }).finally(()=>{
-            setSubmitting(false)
-        })    
-
+            });
+        }).finally(() => {
+            setSubmitting(false);
+        });
     }
 
     return (
         <div className="desktop">
             <div className="group-wrapper">
                 <div className="group">
-                    <div className="overlap" style={{display:'flex',justifyContent:'center'}}>
+                    <div className="overlap" style={{ display: 'flex', justifyContent: 'center' }}>
                         <div className="overlap-group">
                             <div className="overlap-group-wrapper">
                                 <div className="div">
@@ -52,37 +53,28 @@ export const SignUp = () => {
                                 </div>
                             </div>
                             <div>
-                            <p className="sign-up">
-                                <span className="span"></span>
-                                <span className="text-wrapper-2">Sign Up</span>
-                            </p>
-                          
-                        </div>
+                                <p className="sign-up">
+                                    <span className="span"></span>
+                                    <span className="text-wrapper-2">Sign Up</span>
+                                </p>
+                            </div>
                         </div>
                         <Formik
-                            initialValues={{ username :'',email: '', password: '',confirmpassword:''}}
+                            initialValues={{ username: '', email: '', password: '', confirmpassword: '' }}
                             validate={values => {
                                 const errors = {};
-                                if(!values.username)
-                                {
-                                    errors.username='*Username is Required'
-                                }
-                                 else if (!values.email) {
+                                if (!values.username) {
+                                    errors.username = '*Username is Required';
+                                } else if (!values.email) {
                                     errors.email = 'Email is Required';
                                 } else if (
                                     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
                                 ) {
                                     errors.email = 'Invalid email address';
-                                }
-                                else if(
-                                    values.password===''
-                                )
-                                {
-                                    errors.password='*Password is Required'
-                                }
-                                else if(values.password!=values.confirmpassword)
-                                {
-                                    errors.confirmpassword='Passwords donot Match'
+                                } else if (values.password === '') {
+                                    errors.password = '*Password is Required';
+                                } else if (values.password !== values.confirmpassword) {
+                                    errors.confirmpassword = 'Passwords do not Match';
                                 }
                                 return errors;
                             }}
@@ -90,27 +82,23 @@ export const SignUp = () => {
                         >
                             {({ isSubmitting }) => (
                                 <Form className='login-form' style={{ display: "flex", flexDirection: 'column', justifyContent: "center" }}>
-                                     <Field className="field" type="text" name="username" placeholder='UserName' />
-                                     <ErrorMessage name="username">{msg => <div style={{color:'red', fontSize:14}}>{msg}</div>}</ErrorMessage>
+                                    <Field className="field" type="text" name="username" placeholder='UserName' />
+                                    <ErrorMessage name="username">{msg => <div style={{ color: 'red', fontSize: 14 }}>{msg}</div>}</ErrorMessage>
                                     <Field className="field" type="email" name="email" placeholder='Email' />
-                                    <ErrorMessage name="email">{msg => <div style={{color:'red', fontSize:14}}>{msg}</div>}</ErrorMessage>
+                                    <ErrorMessage name="email">{msg => <div style={{ color: 'red', fontSize: 14 }}>{msg}</div>}</ErrorMessage>
                                     <Field className="field" type="password" name="password" placeholder='Password' />
-                                    <ErrorMessage name="password">{msg => <div style={{color:'red', fontSize:14}}>{msg}</div>}</ErrorMessage>
+                                    <ErrorMessage name="password">{msg => <div style={{ color: 'red', fontSize: 14 }}>{msg}</div>}</ErrorMessage>
                                     <Field className="field" type="password" name="confirmpassword" placeholder='Confirm Password' />
-                                    <ErrorMessage name="confirmpassword">{msg => <div style={{color:'red', fontSize:14}}>{msg}</div>}</ErrorMessage>
-                                    <button disabled={isSubmitting} type='submit' style={{ width: 198, height: 71, alignSelf: 'center', background: 'linear-gradient(0deg, #4584FF 0%, #4584FF 100%), linear-gradient(0deg, rgba(0, 0, 0, 0.20) 0%, rgba(0, 0, 0, 0.20) 100%)', borderRadius: 10, color: 'white',cursor:'pointer' }}>Sign Up </button>
-                                    <div style={{color: 'black', fontSize: 16, fontFamily: 'Plus Jakarta Sans', fontWeight: '400', wordWrap: 'break-word', alignSelf: 'center', marginTop: 20}}>Already an account exists?<Link style={{width: '100%', color: '#4895EF', fontSize: 16, fontFamily: 'Plus Jakarta Sans', fontWeight: '300', wordWrap: 'break-word',cursor:'pointer'}} to ={'/login'}>Login</Link></div> 
-                                </Form>                             
+                                    <ErrorMessage name="confirmpassword">{msg => <div style={{ color: 'red', fontSize: 14 }}>{msg}</div>}</ErrorMessage>
+                                    <button disabled={isSubmitting} type='submit' style={{ width: 198, height: 71, alignSelf: 'center', background: 'linear-gradient(0deg, #4584FF 0%, #4584FF 100%), linear-gradient(0deg, rgba(0, 0, 0, 0.20) 0%, rgba(0, 0, 0, 0.20) 100%)', borderRadius: 10, color: 'white', cursor: 'pointer' }}>Sign Up </button>
+                                    <div style={{ color: 'black', fontSize: 16, fontFamily: 'Plus Jakarta Sans', fontWeight: '400', wordWrap: 'break-word', alignSelf: 'center', marginTop: 20 }}>Already an account exists?<Link style={{ width: '100%', color: '#4895EF', fontSize: 16, fontFamily: 'Plus Jakarta Sans', fontWeight: '300', wordWrap: 'break-word', cursor: 'pointer' }} to={'/login'}>Login</Link></div>
+                                </Form>
                             )}
                         </Formik>
-                        <ToastContainer/>
-                       
+                        <ToastContainer />
                     </div>
-                    
                 </div>
-
             </div>
         </div>
-        
     );
 };
