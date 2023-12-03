e60922bb39f46b0a000724ee740a14f04af35067
Manish Manda <manishmanda@my.unt.edu>
2023-12-03 13:55:08 -0600

    changes undo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import Login from './Pages/Login'
 import {SignUp} from './Pages/SignUp'
 import './App.css';
@@ -6,9 +5,7 @@ import {Routes, Route ,Navigate} from 'react-router-dom'
 import LearningVideos from './Pages/LearningVideos';
 import NotFound from './Pages/NotFound';
 import Home  from './Pages/Home';
-
 import Footer from './components/Footer';
-import { Header } from './components/Header';
 import ContentPage from './Pages/ContentPage';
 import ProtectedRoutes from './ProtectedRoutes'
 import CourseCompletion from './Pages/CourseCompletion';
@@ -23,7 +20,7 @@ function App() {
       <Route exact path='/' element={<Home/>}/>
       <Route exact path='/sign-up' element={<SignUp/>}/>
       <Route exact  path='/login' element={<Login/>}/>
-      <Route exact path='/learning-videos' element={<ProtectedRoutes><LearningVideos/></ProtectedRoutes>}/>
+      <Route exact path='/learning-videos' element={<LearningVideos/>}/>
       <Route exact path='/notfound' element={<NotFound/>}/>
       <Route exact path='/home' element={<Home/>}/>
       <Route exact path='/learn' element={<ContentPage/>}></Route>
